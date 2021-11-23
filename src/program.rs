5ab040de94a3e378f6d5cca9dd054545cd2b3f38
Paul Butler <paulgb@gmail.com>
2021-11-23 18:35:19 -0500

    program is enum

--- src/program.rs
+++ src/program.rs
@@ -3,12 +3,75 @@ use anyhow::{anyhow, Result};
 use std::marker::PhantomData;
 use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader, WebGlVertexArrayObject};
 
-pub struct Program<T: VertexAttribute> {
-    frag_shader_src: String,
-    vert_shader_src: String,
+pub enum Program<T: VertexAttribute> {
+    Uncompiled {
+        frag_shader_src: String,
+        vert_shader_src: String,
+    },
+    Compiled {
+        program: WebGlProgram,
+        vao: WebGlVertexArrayObject,
+        _ph: PhantomData<T>,
+    },
+}
 
-    program_and_vao: Option<(WebGlProgram, WebGlVertexArrayObject)>,
-    _ph: PhantomData<T>,
+impl<T: VertexAttribute> Program<T> {
+    pub fn new(frag_shader: &str, vert_shader: &str) -> Self {
+        Program::Uncompiled {
+            frag_shader_src: frag_shader.to_string(),
+            vert_shader_src: vert_shader.to_string(),
+        }
+    }
+
+    pub fn bind(&mut self, gl: &WebGl2RenderingContext) -> Result<()> {
+        match self {
+            Program::Compiled { program, vao, .. } => {
+                gl.use_program(Some(program));
+                gl.bind_vertex_array(Some(vao));
+            }
+            Program::Uncompiled {
+                vert_shader_src,
+                frag_shader_src,
+            } => {
+                let frag_shader =
+                    compile_shader(gl, ShaderType::FragmentShader, &frag_shader_src)?;
+                let vertex_shader =
+                    compile_shader(gl, ShaderType::VertexShader, &vert_shader_src)?;
+
+                let program = gl
+                    .create_program()
+                    .ok_or_else(|| anyhow!("Error creating program."))?;
+
+                gl.attach_shader(&program, &frag_shader);
+                gl.attach_shader(&program, &vertex_shader);
+
+                gl.link_program(&program);
+
+                if !gl.get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS) {
+                    if let Some(info) = gl.get_program_info_log(&program) {
+                        return Err(anyhow!("Encountered error linking program: {}", info));
+                    } else {
+                        return Err(anyhow!("Encountered unknown error linking program."));
+                    }
+                }
+
+                let vao = gl
+                    .create_vertex_array()
+                    .ok_or_else(|| anyhow!("Couldn't create vertex array."))?;
+
+                gl.bind_vertex_array(Some(&vao));
+                bind_vertex_attributes(&T::describe(), &gl, &program)?;
+
+                *self = Program::Compiled {
+                    program,
+                    vao,
+                    _ph: PhantomData::default(),
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -40,57 +103,6 @@ fn compile_shader(
             .get_shader_info_log(&shader)
             .map(|d| anyhow!("Error compiling shader {}", d))
             .unwrap_or_else(|| anyhow!("Unknown error compiling shader.")))
-    }
-}
-
-impl<T: VertexAttribute> Program<T> {
-    pub fn new(frag_shader: &str, vert_shader: &str) -> Self {
-        Program {
-            program_and_vao: None,
-            _ph: PhantomData::default(),
-            frag_shader_src: frag_shader.to_string(),
-            vert_shader_src: vert_shader.to_string(),
-        }
-    }
-
-    pub fn bind(&mut self, gl: &WebGl2RenderingContext) -> Result<()> {
-        if let Some((program, vao)) = self.program_and_vao.as_ref() {
-            gl.use_program(Some(program));
-            gl.bind_vertex_array(Some(vao));
-        } else {
-            let frag_shader =
-                compile_shader(gl, ShaderType::FragmentShader, &self.frag_shader_src)?;
-            let vertex_shader =
-                compile_shader(gl, ShaderType::VertexShader, &self.vert_shader_src)?;
-
-            let program = gl
-                .create_program()
-                .ok_or_else(|| anyhow!("Error creating program."))?;
-
-            gl.attach_shader(&program, &frag_shader);
-            gl.attach_shader(&program, &vertex_shader);
-
-            gl.link_program(&program);
-
-            if !gl.get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS) {
-                if let Some(info) = gl.get_program_info_log(&program) {
-                    return Err(anyhow!("Encountered error linking program: {}", info));
-                } else {
-                    return Err(anyhow!("Encountered unknown error linking program."));
-                }
-            }
-
-            let vertex_array = gl
-                .create_vertex_array()
-                .ok_or_else(|| anyhow!("Couldn't create vertex array."))?;
-
-            gl.bind_vertex_array(Some(&vertex_array));
-            bind_vertex_attributes(&T::describe(), &gl, &program)?;
-
-            self.program_and_vao = Some((program, vertex_array));
-        }
-
-        Ok(())
     }
 }
 
--- src/state/mod.rs
+++ src/state/mod.rs
@@ -5,10 +5,8 @@ use self::blending::BlendFunc;
 pub mod blending;
 pub mod enable;
 
-#[derive(Default)]
-pub struct WebGLState<T: VertexAttribute> {
+pub struct Stage<T: VertexAttribute> {
     pub blend_func: Option<BlendFunc>,
-    pub program: Option<Program<T>>,
-
-    pub buffer: Option<AttributeBuffer<T>>,
+    pub program: Program<T>,
+    pub buffer: AttributeBuffer<T>,
 }
