ccc73b0878fe8090a64deb742ce3a433ffb1c341
paulgb <paulgb@gmail.com>
2021-12-12 01:28:39 +0000

    Deploying to gh-pages from @ drifting-in-space/limelight@e7d92403d2e7f04de117eaa48939fdd8811ca974 🚀

--- 01-triangle/index-d73db5440ea97b51.js
+++ 01-triangle/index-d73db5440ea97b51.js
@@ -259,7 +259,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-428b3e91aa65f3d4_bg.wasm', import.meta.url);
+        input = new URL('index-d73db5440ea97b51_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
