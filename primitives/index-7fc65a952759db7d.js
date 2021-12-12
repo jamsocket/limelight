9e2c1035576412664bfb4c3aba79456676e7777e
paulgb <paulgb@gmail.com>
2021-12-12 04:02:01 +0000

    Deploying to gh-pages from @ drifting-in-space/limelight@8ff024aabb966393b27869a2cff5e7e0fd3c554d 🚀

--- primitives/index-7fc65a952759db7d.js
+++ primitives/index-7fc65a952759db7d.js
@@ -259,7 +259,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-311f1ff84ec66903_bg.wasm', import.meta.url);
+        input = new URL('index-7fc65a952759db7d_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
